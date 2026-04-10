3edf345ea892469e986ee54e380dddc7f33d8a2c
ms-anhhh810 <your-email@example.com>
2026-04-10 13:07:54 +0700

    hotfix: add AiClub find by name

--- backend/src/main/java/com/example/football/repository/AiClubRepository.java
+++ backend/src/main/java/com/example/football/repository/AiClubRepository.java
@@ -5,8 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface AiClubRepository extends JpaRepository<AiClub, Long> {
     List<AiClub> findByLeagueTier(Integer leagueTier);
+    Optional<AiClub> findByName(String name);
 }
